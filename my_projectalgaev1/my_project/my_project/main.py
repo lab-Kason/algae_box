9d85bb2c65f33f11435fd6511dd78d5e9e0e5cd9
lab-Kason <loginnamekason@gmail.com>
2026-05-29 16:46:18 +0800

    f

---
+++ my_projectalgaev1/my_project/my_project/main.py
@@ -0,0 +1,16 @@
+
+
+from jitx.circuit import Circuit
+from jitx.sample import SampleDesign
+from jitxlib.parts import ResistorQuery, Resistor
+from jitx.units import ohm, kohm
+
+class Resistors(Circuit):
+  r1 = Resistor(resistance=100 * ohm)
+  r2 = Resistor(resistance=2 * kohm)
+  nets = [r1.p1 + r2.p1,
+          r1.p2 + r2.p2]
+
+class my_project(SampleDesign):
+  resistor_defaults = ResistorQuery(case="0402", tolerance=0.01)
+  circuit=Resistors()
